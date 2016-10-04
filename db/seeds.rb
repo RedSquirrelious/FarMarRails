8080ae11273da1bd820b7a98b349a1a844df771a
Nina Mutty <ninamutty@gmail.com>
2016-10-04 16:23:03 -0700

    seeded files and edited changes in text file that excel made

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+CSV.foreach('seed_csvs/markets.csv', :headers => true) do |csv_obj|
+  Market.create(id: csv_obj['id'].to_i, name: csv_obj['name'], street: csv_obj['street'], city: csv_obj['city'], county: csv_obj['county'], state: csv_obj['state'], zip: csv_obj['zip'])
+end
+
+CSV.foreach('seed_csvs/vendors.csv', :headers => true) do |csv_obj|
+  Vendor.create(id: csv_obj['id'].to_i, name: csv_obj['name'], num_employees: csv_obj['num_employees'])
+end
+
+CSV.foreach('seed_csvs/market_vendors_clean.csv', :headers => false) do |csv_obj|
+  MarketVendor.create(markets_id: csv_obj[0], vendors_id: csv_obj[1])
+end
