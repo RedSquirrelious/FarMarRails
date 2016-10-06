47728bb39fa2f917d6f21e7d1703062c8ac960b7
Sassa Kitka <sassa.kitka@gmail.com>
2016-10-06 14:47:29 -0700

    added show item type method

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,17 @@
 class LandingController < ApplicationController
   def index
   end
+
+  def show_item_type
+  	@products = Product.all
+
+  	@types = []
+
+  	@products.each do |product|
+  		type = product.split(/\W+/)
+  		@types << type[1]
+  	end
+  	
+  	return @types
+  end
 end
