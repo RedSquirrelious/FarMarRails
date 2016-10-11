d98b0290fc0fe2aff055260c4f31e385b260835d
Sassa Kitka <sassa.kitka@gmail.com>
2016-10-11 10:57:55 -0700

    added rotate image and rotate quote methods to vendor controller

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -160,4 +160,16 @@ class VendorsController < ApplicationController
 
   def total_sales
   end
+
+  def rotate_image
+    celebrity = RandomCelebritySighting.all.sample
+    @celebrity_image = celebrity.image_url
+    @celebrity_name = celebrity.name
+    return @celebrity_image, @celebrity_name
+  end
+
+  def rotate_quote
+    @quote = RandomQuote.all.sample.quote
+  end
+
 end
